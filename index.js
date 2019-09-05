dc37f4acda51703ab1fd3ac5e9c84b9016e2e43c
jlabung <labungjugal1 @gmail.com>
2019-09-06 00:26:48 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,98 @@
+const modal = document.querySelector(".modal_background");
+modal.addEventListener("click", () => {
+	modal.classList.add("hide");
+})
+
+
+fetch("http://kea-alt-del.dk/t5/api/categories")
+	.then(function (response) {
+		return response.json()
+	}).then(function (data) {
+		data.forEach(buildCategory);
+		getProducts();
+	})
+
+function buildCategory(data) {
+	//	console.log(data)
+	const section = document.createElement("section");
+	const header = document.createElement("h1");
+	header.textContent = data;
+	section.setAttribute("id", data);
+	section.appendChild(header);
+
+	document.querySelector("main").appendChild(section);
+	console.log(data);
+}
+
+function getProducts() {
+
+	fetch("https://kea-alt-del.dk/t5/api/productlist")
+		.then(function (response) {
+			return response.json()
+		}).then(function (data) {
+			data.forEach(showCourse)
+		})
+
+}
+
+function showCourse(course) {
+	console.log(course)
+
+	const template = document.querySelector("template").content;
+	const myCopy = template.cloneNode("true");
+	myCopy.querySelector(".data_img").src = "imgs/imgs/large/" + course.image + ".jpg";
+
+	myCopy.querySelector(".data_name").textContent = course.name;
+	myCopy.querySelector(".data_price").textContent = course.price;
+	if (course.discount) {
+		myCopy.querySelector(".data_price").classList.add("discount");
+		myCopy.querySelector(".data_discount").textContent = Math.round(course.price - course.discount / 100 * course.price)
+
+	} else {
+		myCopy.querySelector(".data_discount").remove();
+	}
+
+	if (!course.soldout) {
+		myCopy.querySelector("article").classList.remove("soldOut");
+	}
+
+	myCopy.querySelector(".containAlcohol").textContent = "Alcohol:"+course.alcohol+";";
+
+	myCopy.querySelector(".vegeterian").textContent = "Vegetarian:"+course.vegetarian+";";
+
+
+	myCopy.querySelector("button").addEventListener("click", () => {
+		fetch(`https://kea-alt-del.dk/t5/api/product?id=${course.id}`)
+			.then(res => res.json())
+			.then(showDetails);
+
+	});
+
+	function showDetails(data) {
+		modal.querySelector(".modalName").textContent = course.name;
+
+		modal.querySelector(".modal_image").src = "imgs/imgs/small/" + data.image + "-sm.jpg";
+
+		modal.querySelector(".description").textContent = course.shortdescription;
+
+		modal.querySelector(".modalPrice").textContent = course.price;
+
+
+
+
+		modal.classList.remove("hide");
+	}
+
+	document.querySelector(`#${course.category}`).appendChild(myCopy)
+}
+
+
+
+
+
+function addCopy() {
+	const myTemplate = document.querySelector("template").content;
+	const myCopy = myTemplate.cloneNode(true);
+	const main = document.querySelector("aside");
+	aside.appendChild(myCopy)
+}
